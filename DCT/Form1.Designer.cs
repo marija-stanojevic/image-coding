c2a568f07dcacb4f56b10abaa697b7f3c89f2051
Marija Stanojevic <marija.stanojevic@temple.edu>
2018-03-05 16:57:41 -0800

    Add files via upload

---
+++ DCT/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace DCT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(662, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Naloži";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(662, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Kodiraj";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(325, 359);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(331, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(325, 359);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(657, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nastavi stopu stiskanja";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(662, 157);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(660, 214);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(188, 147);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 366);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ DCT/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace DCT
+{
+    public partial class Form1 : Form
+    {
+        double pi = 3.14;
+        Bitmap bmp;
+        byte[] data;
+        sbyte[] sdata;
+        double[,,,] cosm;
+        int w, h;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "(*.bmp)|*.bmp";
+            openFileDialog1.FileName = "";
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName!=""){
+                FileInfo fI=new FileInfo(openFileDialog1.FileName);
+                bmp = new Bitmap(openFileDialog1.FileName);
+                if (bmp.Height % 8 != 0) h = bmp.Height - bmp.Height % 8 + 8;
+                else h=bmp.Height;
+                if (bmp.Width % 8 != 0) w =bmp.Width - bmp.Width % 8 + 8;
+                else w= bmp.Width;
+                data = new byte[fI.Length];
+                sdata = new sbyte[h*w];
+                using (FileStream fs = fI.OpenRead())
+                {
+                    fs.Read(data, 0, data.Length);
+                }
+                for (int i = 0; i < h; i++)
+                    for (int j = 0; j < w; j++)
+                    {
+                        if (i<bmp.Height && j<bmp.Width)
+                        {
+                            sdata[i*w+j] = (sbyte)(data[1078+i*bmp.Width+j] - 128);
+                        }
+                        else {
+                            sdata[i*w+j]=0;
+                        }                                               
+                    }                       
+                }
+                pictureBox1.Image=bmp;
+                bmp.Save("kodirano.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage; 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool[] kod= new bool[h * w * 8];
+            int[] dct;
+            Stopwatch sw=new Stopwatch();
+            sw.Start();
+            dct=new int[w*h];
+            int pp = 0;
+            try
+            {
+                pp = int.Parse(textBox1.Text);
+            }
+            catch {
+                pp = 0;
+            }
+            cosm=new double[8,8,8,8];
+            for (int ii = 0; ii < 8; ii++)
+                for (int jj=0; jj<8; jj++)
+                    for (int ik=0; ik<8; ik++)
+                        for (int jk=0; jk<8; jk++)
+                        {
+                            cosm[ii, jj, ik, jk] = Math.Cos((2 * ik + 1) * ii * pi / 16.0) * Math.Cos((2 * jk + 1) * jj * pi / 16.0);
+                        }
+            for (int i = 0; i < w; i += 8)
+                for (int j = 0; j < h; j += 8)
+                    for (int ii = 0; ii < 8; ii++)
+                        for (int jj = 0; jj < 8; jj++)
+                        {
+                            if (15 - ii - jj <= pp)
+                            {
+                                dct[(j + jj) * w + i + ii] = 0;
+                            }
+                            else
+                            {
+                                float c = 0;
+                                double dd = 0;
+                                if (ii == 0 && jj == 0) c = (float)0.5;
+                                else if (ii != 0 && jj != 0) c = 1;
+                                else c = (float)(1 / Math.Sqrt(2));
+                                for (int ik = 0; ik < 8; ik++)
+                                    for (int jk = 0; jk < 8; jk++)
+                                    {
+                                        dd += sdata[(j + jk) * w + i + ik] * cosm[ii, jj, ik, jk];
+                                    }
+                                dct[(j + jj) * w + i + ii] = (int)(c * dd / 4);
+                            }
+                        }
+            for (int i = 0; i < 4; i++)
+            {
+                int t = pp;
+                if (t % 2 == 0) kod[3 - i] = false;
+                else kod[3 - i] = true;
+                t /= 2;
+            }
+           int br=4;
+           int nul = 0; 
+           for (int i = 0; i < w; i += 8)
+                for (int j = 0; j < h; j += 8)
+                {
+                    for (int ii = 0; ii < 8; ii++)
+                        for (int jj = 0; jj < 8; jj++)
+                        {
+                            if (15 - ii - jj > pp)
+                            {
+                                if (ii + jj == 0)
+                                {
+                                    for (int k = 0; k < 10; k++)
+                                    {
+                                        int t = dct[(j + jj) * w + i + ii];
+                                        if (t % 2 == 0) kod[11 + br - k] = false;
+                                        else kod[11 + br - k] = true;
+                                        t /= 2;
+                                    }
+                                    if (dct[(j + jj) * w + i + ii] > 0) kod[br] = false;
+                                    else kod[br] = true;
+                                    br += 11;
+                                }
+                                if (dct[(j + jj) * w + i + ii] == 0) nul++;
+                                else
+                                {
+                                    if (nul > 0)
+                                    {
+                                        kod[br++] = false;
+                                        for (int k = 0; k < 6; k++)
+                                        {
+                                            int t = nul;
+                                            nul = 0;
+                                            if (t % 2 == 0) kod[6 + br - k] = false;
+                                            else kod[6 + br - k] = true;
+                                            t /= 2;
+                                        }
+                                        br += 6;
+                                        int dol = 0;
+                                        int tt = dct[(j + jj) * w + i + ii];
+                                        while (tt > 0)
+                                        {
+                                            dol++;
+                                            tt /= 2;
+                                        }
+                                        dol++;
+                                        for (int k = 0; k < dol-1; k++)
+                                        {
+                                            tt = dct[(j + jj) * w + i + ii];
+                                            if (tt % 2 == 0) kod[dol + br - k] = false;
+                                            else kod[dol + br - k] = true;
+                                            tt /= 2;
+                                        }
+                                        if (dct[(j + jj) * w + i + ii] > 0) kod[br] = false;
+                                        else kod[br] = true;
+                                        br += dol;
+                                    }
+                                    else
+                                    {
+                                        kod[br++] = true;
+                                        int dol = 0;
+                                        int tt = dct[(j + jj) * w + i + ii];
+                                        while (tt > 0)
+                                        {
+                                            dol++;
+                                            tt /= 2;
+                                        }
+                                        dol++;
+                                        for (int k = 0; k < dol; k++)
+                                        {
+                                            tt = dct[(j + jj) * w + i + ii];
+                                            if (tt % 2 == 0) kod[dol + br - k] = false;
+                                            else kod[dol + br - k] = true;
+                                            tt /= 2;
+                                        }
+                                        if (dct[(j + jj) * w + i + ii] > 0) kod[br] = false;
+                                        else kod[br] = true;
+                                        br += dol;
+                                    }
+                                }
+                            }
+                        }
+                    if (nul > 0)
+                    {
+                        kod[br++] = false;
+                        for (int k = 0; k < 6; k++)
+                        {
+                            int t = nul;
+                            nul = 0;
+                            if (t % 2 == 0) kod[6 + br - k] = false;
+                            else kod[6 + br - k] = true;
+                            t /= 2;
+                        }
+                        br += 6;
+                    }
+                    nul = 0;
+                }
+            int tbr=br;
+           if (br%8>0)
+           {
+               int t = br;
+               for (int i=0; i<8-(t%8); i++)
+               {
+                   if (i==0) kod[br++]=true;
+                   else kod[br++]=false;
+               }
+           }
+           else {
+               for (int i=0; i<8; i++)
+               {
+                   if (i==0) kod[br++]=true;
+                   else kod[br++]=false;
+               }           
+           }
+           byte[] kon = new byte[br / 8];
+           for (int i = 0; i < br / 8; i++)
+           {
+                kon[i] = 0;
+                for (int j = 0; j < 8; j++)
+                {
+                   kon[i] *= 2;
+                   if (kod[i*8+j]) kon[i] += 1;
+                }
+            }
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+            string eT = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
+            richTextBox1.Text = "Čas kodiranja je: " + eT + "\n" + "Velikost originalne slike je: " + data.Length + " zlogov\n" + "Velikost shranjene slike je: " + kon.Length + " zlogov\n" + "Število bitov v kodiranoj slici je: " + tbr + "\n" + "Primerjava velikosti je: "+ 12.5*tbr/data.Length+" procentov";
+            /* TypeConverter tr=TypeDescriptor.GetConverter(typeof(Bitmap));
+            Bitmap bit1=(Bitmap)tr.ConvertFrom(kon);
+            bit1.Save("kodirano.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+            bit1.Save("kodirano.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);*/
+            File.WriteAllBytes("kodirano.txt", kon);
+        }
+    }
+}
